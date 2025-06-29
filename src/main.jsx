ede120ea3591a1eb7773db6f7669bf3fe639b775
OmkarDorle-27 <94429330+OmkarDorle-27@users.noreply.github.com>
2025-06-29 12:47:44 +0530

    h

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => {
-  return <h1>Hello, React with JSX!</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
